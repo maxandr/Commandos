43ae107c9d26bd62202f0247f8d7b90ad223bb33
maxandr <maxandryo@gmail.com>
2013-09-14 23:53:13 +0900

    Zoom fixed

--- Assets/Scripts/PinchZoom.cs
+++ Assets/Scripts/PinchZoom.cs
@@ -7,65 +7,78 @@
 using UnityEngine;
 using System.Collections;
  
-public class PinchZoom : TouchLogic 
+public class PinchZoom : TouchLogic
 {
- public float zoomSpeed = 5.0f;
- 
- //buckets for caching our touch positions
- private Vector2 currTouch1 = Vector2.zero,
+	public float zoomSpeed = 5.0f;
+	public float MIN_ZOOM = 10.0f;
+	public float MAX_ZOOM = 50.0f;
+	
+	//buckets for caching our touch positions
+	private Vector2 currTouch1 = Vector2.zero,
  lastTouch1 = Vector2.zero,
  currTouch2 = Vector2.zero,
  lastTouch2 = Vector2.zero;
  
- //used for holding our distances and calculating our zoomFactor
- private float currDist = 0.0f,
+	//used for holding our distances and calculating our zoomFactor
+	private float currDist = 0.0f,
  lastDist = 0.0f,
  zoomFactor = 0.0f;
  
- void OnTouchMovedAnywhere()
- {
-  Zoom();
- }
- void OnTouchStayAnywhere()
- {
-  Zoom();
- }
- 
- //find distance between the 2 touches 1 frame before & current frame
- //if the delta distance increased, zoom in, if delta distance decreased, zoom out
- void Zoom()
- {
-  //Caches touch positions for each finger
-  switch(TouchLogic.currTouch)
-  {
-   case 0://first touch
-    currTouch1 = Input.GetTouch(0).position;
-    lastTouch1 = currTouch1 - Input.GetTouch(0).deltaPosition;
-    break;
-   case 1://second touch
-    currTouch2 = Input.GetTouch(1).position;
-    lastTouch2 = currTouch2 - Input.GetTouch(1).deltaPosition;
-    break;
-  }
+	void OnTouchMovedAnywhere ()
+	{
+		Zoom ();
+	}
+
+	void OnTouchStayedAnywhere ()
+	{
+		Zoom ();
+	}
+
+	void OnTouchBeganAnywhere ()
+	{
+		
+	}
+
+	void OnTouchEndedAnywhere ()
+	{
+		
+	}
+
+	//find distance between the 2 touches 1 frame before & current frame
+	//if the delta distance increased, zoom in, if delta distance decreased, zoom out
+	void Zoom ()
+	{
+		//Caches touch positions for each finger
+		switch (TouchLogic.currTouch) {
+		case 0://first touch
+			currTouch1 = Input.GetTouch (0).position;
+			lastTouch1 = currTouch1 - Input.GetTouch (0).deltaPosition;
+			break;
+		case 1://second touch
+			currTouch2 = Input.GetTouch (1).position;
+			lastTouch2 = currTouch2 - Input.GetTouch (1).deltaPosition;
+			break;
+		}
   
-  //finds the distance between your moved touches
-  //we dont want to find the distance between 1 finger and nothing
-  if(TouchLogic.currTouch >= 1)
-  {
-   currDist = Vector2.Distance(currTouch1, currTouch2);
-   lastDist = Vector2.Distance(lastTouch1, lastTouch2);
-  }
-  else
-  {
-   currDist = 0.0f;
-   lastDist = 0.0f;
-  }
-  
-  //Calculate the zoom magnitude
-  zoomFactor = Mathf.Clamp( lastDist - currDist, 10.0f, 40.0f);
-  
-  //apply zoom to our camera
-  Camera.mainCamera.transform.Translate(Vector3.forward * zoomFactor * zoomSpeed * Time.deltaTime);
-  
- }
+		//finds the distance between your moved touches
+		//we dont want to find the distance between 1 finger and nothing
+		if (Input.touchCount == 2) {
+			if (TouchLogic.currTouch >= 1) {
+				currDist = Vector2.Distance (currTouch1, currTouch2);
+				lastDist = Vector2.Distance (lastTouch1, lastTouch2);
+			} else {
+				currDist = 0.0f;
+				lastDist = 0.0f;
+			}
+			//Calculate the zoom magnitude
+			zoomFactor = Mathf.Clamp (lastDist - currDist, -30.0f, 30.0f);
+			Debug.Log ("zoomFactor: " + currTouch);
+			//apply zoom to our camera
+			Camera.mainCamera.transform.Translate (Vector3.back * zoomFactor * zoomSpeed * Time.deltaTime);
+			Vector3 _temp = Camera.mainCamera.transform.position;
+			float _y = Mathf.Clamp (_temp.y, MIN_ZOOM, MAX_ZOOM);
+			_temp.y = _y;
+			Camera.mainCamera.transform.position = _temp;
+		}
+	}
 }
--- Assets/Scripts/TouchLogic.cs
+++ Assets/Scripts/TouchLogic.cs
@@ -7,63 +7,51 @@
 using UnityEngine;
 using System.Collections;
  
-public class TouchLogic : MonoBehaviour 
+public class TouchLogic : MonoBehaviour
 {
- public static int currTouch = 0;//so other scripts can know what touch is currently on screen
- private Ray ray;//this will be the ray that we cast from our touch into the scene
- private RaycastHit rayHitInfo = new RaycastHit();//return the info of the object that was hit by the ray
+	public static int currTouch = 0;//so other scripts can know what touch is currently on screen
+	private Ray ray;//this will be the ray that we cast from our touch into the scene
+	private RaycastHit rayHitInfo = new RaycastHit ();//return the info of the object that was hit by the ray
  
- void Update () 
- {
-  //is there a touch on screen?
-  if(Input.touches.Length <= 0)
-  {
-   //if no touches then execute this code
-  }
-  else //if there is a touch
-  {
-   //loop through all the the touches on screen
-   for(int i = 0; i < Input.touchCount; i++)
-   {
-    currTouch = i;
-    //executes this code for current touch (i) on screen
-    if(this.guiTexture != null && (this.guiTexture.HitTest(Input.GetTouch(i).position)))
-    {
-     //if current touch hits our guitexture, run this code
-     if(Input.GetTouch(i).phase == TouchPhase.Began)
-     {
-      //need to send message b/c function is not present in this script
-      this.SendMessage("OnTouchBegan");
-      //OnTouchBegan(); //cant do this b/c the function is not defined in this script
-     }
-     if(Input.GetTouch(i).phase == TouchPhase.Ended)
-     {
-      this.SendMessage("OnTouchEnded");
-     }
-     if(Input.GetTouch(i).phase == TouchPhase.Moved)
-     {
-      this.SendMessage("OnTouchMoved");
-     }
-    }
+	void Update ()
+	{
+		//is there a touch on screen?
+		if (Input.touches.Length <= 0) {
+			//if no touches then execute this code
+		} else { //if there is a touch
+			//loop through all the the touches on screen
+			for (int i = 0; i < Input.touchCount; i++) {
+				currTouch = i;
+				//executes this code for current touch (i) on screen
+				if (this.guiTexture != null && (this.guiTexture.HitTest (Input.GetTouch (i).position))) {
+					//if current touch hits our guitexture, run this code
+					if (Input.GetTouch (i).phase == TouchPhase.Began) {
+						//need to send message b/c function is not present in this script
+						this.SendMessage ("OnTouchBegan");
+						//OnTouchBegan(); //cant do this b/c the function is not defined in this script
+					}
+					if (Input.GetTouch (i).phase == TouchPhase.Ended) {
+						this.SendMessage ("OnTouchEnded");
+					}
+					if (Input.GetTouch (i).phase == TouchPhase.Moved) {
+						this.SendMessage ("OnTouchMoved");
+					}
+				}
     
-    //outside so it doesn't require the touch to be over the guitexture
-    if(Input.GetTouch(i).phase == TouchPhase.Began)
-    {
-     this.SendMessage("OnTouchBeganAnywhere");
-    }
-    if(Input.GetTouch(i).phase == TouchPhase.Ended)
-    {
-     this.SendMessage("OnTouchEndedAnywhere");
-    }
-    if(Input.GetTouch(i).phase == TouchPhase.Moved)
-    {
-     this.SendMessage("OnTouchMovedAnywhere");
-    }
-    if(Input.GetTouch(i).phase == TouchPhase.Stationary)
-    {
-     this.SendMessage("OnTouchStayedAnywhere");
-    }
-    
+				//outside so it doesn't require the touch to be over the guitexture
+				if (Input.GetTouch (i).phase == TouchPhase.Began) {
+					this.SendMessage ("OnTouchBeganAnywhere");
+				}
+				if (Input.GetTouch (i).phase == TouchPhase.Ended) {
+					this.SendMessage ("OnTouchEndedAnywhere");
+				}
+				if (Input.GetTouch (i).phase == TouchPhase.Moved) {
+					this.SendMessage ("OnTouchMovedAnywhere");
+				}
+				if (Input.GetTouch (i).phase == TouchPhase.Stationary) {
+					this.SendMessage ("OnTouchStayedAnywhere");
+				}
+				/*
     //this is for 3d objects with colliders
     if(Input.GetTouch(i).phase == TouchPhase.Began)
     {
@@ -73,7 +61,8 @@ public class TouchLogic : MonoBehaviour
       rayHitInfo.transform.gameObject.SendMessage("OnTouchBegan3D");
      }
     }
-   }
-  }
- }
+    */
+			}
+		}
+	}
 }
